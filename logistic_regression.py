9c98ddaeefe2fac3cfd9ae0e809844d731306af8
Jishllg Laptop <jishllg@gmail.com>
2021-01-16 02:36:00 -0600

    Generating data for the log regression.

---
+++ logistic_regression.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(12)
+num_observations = 5
+
+x1 = np.random.multivariate_normal([0, 0], [[1, 0.75],[0.75, 1]], num_observations)
+x2 = np.random.multivariate_normal([1, 4], [[1, 0.75],[0.75, 1]], num_observations)
+
+simulated_features = np.vstack((x1, x2)).astype(np.float32)
+simulated_lables = np.hstack((np.zeros(num_observations), np.ones(num_observations)))
+
+plt.figure(figsize=(12, 8))
+plt.scatter((simulated_features[:, 0]), simulated_features[:, 1], c = simulated_lables, alpha = 0.4)
