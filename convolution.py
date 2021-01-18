91cf5eb89595c8107996f666749a8169d402b465
Jishllg Laptop <jishllg@gmail.com>
2021-01-18 10:58:40 -0600

    Importing packages.

---
+++ convolution.py
@@ -0,0 +1,10 @@
+import numpy as np
+from PIL import Image
+from scipy import misc
+from skimage import data
+from skiamage.color import rgb2gray
+import matplotlib.pyplot as plt
+import requests
+from io import StringIO
+from __future__ import division
+
