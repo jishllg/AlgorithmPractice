06251cd8a73161862adac4fd7767818946ae5306
Jishllg Laptop <jishllg@gmail.com>
2021-01-26 13:58:41 -0600

    Recreating the convolution in TF

--- convolution.py
+++ convolution.py
@@ -46,8 +46,20 @@ def conv_2d_kernel(kernel):
 edge_detect = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
 sharpen = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 gaussian_blur = np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]]) / 16
-
-show_img(conv_2d_kernel(gaussian_blur))
+#show_img(conv_2d_kernel(gaussian_blur)) # Uncomment to show kernel transformation
 
 img_arr_4d = img_arr.reshape(-1, img_arr.shape[0], img_arr.shape[1], 1)
 edge_detect_4d = edge_detect.reshape(3, 3, 1, 1)
+
+graph = tf.Graph()
+
+with graph.as_default():
+    tf_img = tf.Variable(np.array(img_arr_4d, dtype = np.float32))
+    tf_edge_kernel = tf.Variable(np.array(edge_detect_4d, dtype = np.float32))
+    tf_convolution_output = tf.nn.conv2d(tf_img, tf_edge_kernel, strides = [1, 1, 1, 1], padding = "SAME")
+
+with tf.Session(graph = graph) as sess:
+    tf.initialize_all_variables().run()
+    transformed_img = tf_convolution_output.eval()
+    transformed_img = transformed_img[0, :, :, 0]
+
