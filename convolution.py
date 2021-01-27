dba694e93c7493f94c0c8e08021fb284f112035a
Jishllg Laptop <jishllg@gmail.com>
2021-01-27 14:30:05 -0600

    Removing TF elements until gaining more knowledge.

--- convolution.py
+++ convolution.py
@@ -46,20 +46,27 @@ def conv_2d_kernel(kernel):
 edge_detect = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
 sharpen = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 gaussian_blur = np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]]) / 16
-#show_img(conv_2d_kernel(gaussian_blur)) # Uncomment to show kernel transformation
+show_img(conv_2d_kernel(gaussian_blur)) # Uncomment to show kernel transformation
 
-img_arr_4d = img_arr.reshape(-1, img_arr.shape[0], img_arr.shape[1], 1)
-edge_detect_4d = edge_detect.reshape(3, 3, 1, 1)
+# Will debug at a future date with greater knowledge of TF
 
-graph = tf.Graph()
+# img_arr_4d = img_arr.reshape(-1, img_arr.shape[0], img_arr.shape[1], 1)
+# edge_detect_4d = edge_detect.reshape(3, 3, 1, 1)
 
-with graph.as_default():
-    tf_img = tf.Variable(np.array(img_arr_4d, dtype = np.float32))
-    tf_edge_kernel = tf.Variable(np.array(edge_detect_4d, dtype = np.float32))
-    tf_convolution_output = tf.nn.conv2d(tf_img, tf_edge_kernel, strides = [1, 1, 1, 1], padding = "SAME")
+# graph = tf.Graph()
 
-with tf.Session(graph = graph) as sess:
-    tf.initialize_all_variables().run()
-    transformed_img = tf_convolution_output.eval()
-    transformed_img = transformed_img[0, :, :, 0]
+# tf.compat.v1.disable_eager_execution()
 
+# with graph.as_default():
+#     tf_img = tf.Variable(np.array(img_arr_4d, dtype = np.float32))
+#     tf_edge_kernel = tf.Variable(np.array(edge_detect_4d, dtype = np.float32))
+#     tf_convolution_output = tf.nn.conv2d(tf_img, tf_edge_kernel, strides = [1, 1, 1, 1], padding = "SAME")
+
+# with tf.compat.v1.Session(graph = graph) as sess:
+#     sess.run(tf.compat.v1.global_variables_initializer())
+#     transformed_img = tf_convolution_output.eval()
+#     transformed_img = transformed_img[0, :, :, 0]
+
+# np.testing.assert_array_almost_equal(conv_2d_kernel(edge_detect), transformed_img, decimal = 4)
+
+# show_img(transformed_img)
