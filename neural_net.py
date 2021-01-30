1839552d42278ed563d8a097fcd6197b5d0b2993
Jishllg Laptop <jishllg@gmail.com>
2021-01-29 23:27:07 -0600

    Preparing data for use with TF.

--- neural_net.py
+++ neural_net.py
@@ -28,3 +28,10 @@ plt.figure(figsize = (12, 8))
 plt.scatter((simulated_features[:, 0]), simulated_features[:, 1], 
             c = simulated_labels, alpha = 0.4)
 plt.show()
+
+# Preparing data
+labels_onehot = np.zeros((simulated_labels.shape[0], 3)).astype(int)
+labels_onehot[np.arrange(len(simulated_labels)), simulated_labels.astype(int)] = 1
+train_dataset, test dataset, \
+train_labels, test_labels = train_test_split(
+    simulated_features, labels_onehot, test_size = 0.1, random_state = 12)
